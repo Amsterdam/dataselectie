80d629d01036985755f9228507e20d50e341061a
Kfir Breger <kfir@cubicalwave.com>
2016-07-01 20:40:50 +0200

    tg-604 Moving the base view class to the generic mixin file. Also making sure that the data is in json format before generating the response

--- web/zelfbediening/datasets/generic/mixins.py
+++ web/zelfbediening/datasets/generic/mixins.py
@@ -1,5 +1,13 @@
+# Python
+import json
+# Packages
+from django.conf import settings
 from django.contrib.gis.geos import GEOSGeometry, Polygon, MultiPolygon
+from django.core import serializers
 from django.db import models
+from django.http import HttpResponse, JsonResponse
+from django.views.generic import ListView
+from elasticsearch import Elasticsearch
 
 
 class ImportStatusMixin(models.Model):
@@ -41,4 +49,155 @@ class CodeOmschrijvingMixin(models.Model):
 
     def __str__(self):
         return "{}: {}".format(self.code, self.omschrijving)
+
+
+#=============================================================
+# Views
+#=============================================================
+
+class TableSearchView(ListView):
+    """
+    A base class to generate tables from search results
+    """
+    #attributes:
+    #---------------------
+    model = None  # The model class to use
+    index = ''  # The name of the index to search in
+    db = None  # The DB to use for the query This allws usage of different dbs
+    keywords = []  # A set of optional keywords to filter the results further
+
+    def __init__(self):
+        self.elastic = Elasticsearch(
+            hosts=settings.ELASTIC_SEARCH_HOSTS, retry_on_timeout=True, refresh=True
+        )
+        self.extra_context_data = None  # Used to store data from elastic used in context
+
+    # Listview methods overloading
+    def get_queryset(self):
+        """
+        This function is called by list view to generate the query set
+        It is overwritten to first use elastic to retrieve the ids then
+        return a queryset based on the ids
+        """
+        # Loading data from elastic. See function for details
+        elastic_data = self.load_from_elastic()
+        # Creating a queryset
+        qs = self.create_queryset(elastic_data)
+        return qs
+
+    def render_to_response(self, context, **response_kwargs):
+        # this can probably be replaced by the json response mixing I had earlier
+        # Need to make sure object list is ready for viewing
+        return HttpResponse(
+            context['object_list'],
+            content_type='application/json',
+            **response_kwargs
+        )
+
+    # Tableview methods
+    def build_elastic_query(self, q):
+        """
+        Builds the dictionary query to send to elastic
+        Parameters:
+        q - The q dict returned from the queries file
+        Returns:
+        The query dict to send to elastic
+        """
+        query = q['Q']
+
+        # Adding filters
+        filters = {}
+        for filter_keyword in self.keywords:
+            val = self.request.GET.get(filter_keyword, None)
+            if val:
+                filters[filter_keyword] = val
+        # If any filters were given, add them
+        if filters:
+            query['filters'] = filters
+            self.elastic_data['filters'] = filters
+
+        # Adding aggregations if given
+        if 'A' in q:
+            for key, aggregatie in q['A']:
+                query['aggs'] = {
+                    'bucket': {
+                        key: val
+                    }
+                }
+        #print('Query:', repr(query))
+        return query
+
+    def load_from_elastic(self):
+        """
+        Loads the data from elastic.
+        It extracts the query parameters from the url and creates a
+        query for elastic. The query returns a list of ids relevant to
+        the search term as well as a list of possible filters, based on
+        aggregates search results. The results are set in the class
+
+        query - the search string
+        term - what type of term this is
+
+        The folowing parameters are optional and can be used
+        to further filter the results
+        postcode - A postcode to limit the results to
+        """
+        # Creating empty result set. Just in case
+        elastic_data = {'ids': [], 'filters': {}}
+        # looking for a query
+        query_string = self.request.GET.get('query', None)
+        term = self.request.GET.get('term', None)
+        if not (query_string and term):
+            print('No query and term given')
+            return elastic_data
+
+        # Building the query
+        q = self.elastic_query(term, query_string)
+        query = self.build_elastic_query(q)
+        # Performing the search
+        response = self.elastic.search(index='zb_bag', body=query)  #, filter_path=['hits.hits._id', 'hits.hits._type'])
+        #print(response)
+        for hit in response['hits']['hits']:
+            elastic_data['ids'].append(hit['_id'])
+        # Enrich result data with neede info
+        self.save_context_data(response)
+        return elastic_data
+
+    def create_queryset(self, elastic_data):
+        """
+        Generates a query set based on the ids retrieved from elastic
+        """
+        ids = elastic_data.get('ids', None)
+        if ids:
+            return self.model.objects.filter(id__in=ids).order_by('_openbare_ruimte_naam').values()[:20]
+        else:
+            # No ids where found
+            return self.model.objects.none().values()
+
+    def get_context_data(self, **kwargs):
+        """
+        Overwrite the context retrital
+        """
+        context = super(TableSearchView, self).get_context_data(**kwargs)
+        context = self.update_context_data(context)
+        return context
+    #===============================================
+    # Context altering functions to be overwritten
+    #===============================================
+    def save_context_data(self, response):
+        """
+        Can be used by the subclass to save extra data to be used
+        later to correct context data
+
+        Parameter:
+        response - the elastic response dict
+        """
+        pass
+
+    def update_context_data(self, context):
+        """
+        Enables the subclasses to update/change the object list
+        """
+        return context
+
 
