50d22cb56d2a8f4e14ad8dc62d9eb75887e949f4
Yigal Duppen <yigal@publysher.nl>
2016-07-21 10:51:46 +0200

    Re-enable /bag/ URLs

--- web/zelfbediening/zelfbediening/urls.py
+++ web/zelfbediening/zelfbediening/urls.py
@@ -1,8 +1,7 @@
 # Packages
 from django.conf.urls import include, url
 
-
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
-#    url(r'^bag/', include('datasets.bag.urls')),
+    url(r'^bag/', include('datasets.bag.urls')),
 ]
