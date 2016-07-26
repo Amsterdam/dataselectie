bbefcc0f25e0f7916f21aeb94c3325d890c85ac2
Kfir Breger <kfir@cubicalwave.com>
2016-07-26 10:46:31 +0200

    Adding health check

--- web/zelfbediening/zelfbediening/urls.py
+++ web/zelfbediening/zelfbediening/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import include, url
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^bag/', include('datasets.bag.urls')),
+    url(r'status/', include('health.urls'))
 ]
