0e5d2c1fa4336497a2022d37337507bd1e6b3427
Yigal Duppen <yigal@publysher.nl>
2016-07-28 10:33:26 +0200

    Use correct context root

--- web/zelfbediening/zelfbediening/urls.py
+++ web/zelfbediening/zelfbediening/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
-    url(r'^bag/', include('datasets.bag.urls')),
-    url(r'status/', include('health.urls'))
+    url(r'^zelfbediening/bag/', include('datasets.bag.urls')),
+    url(r'^status/', include('health.urls'))
 ]
