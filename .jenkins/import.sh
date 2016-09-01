adf403e3232fac0005c1bf5092fa29e6af1826de
Stephan Zaat <s.zaat@amsterdam.nl>
2016-09-01 13:11:04 +0200

    Trying to fix import

--- .jenkins/import.sh
+++ .jenkins/import.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-#set -e
-#set -u
-
 DIR="$(dirname $0)"
 
 dc() {
@@ -17,7 +14,7 @@ mkdir -p ${DIR}/backups
 dc build
 dc up -d database_BAG
 sleep 10 # waiting for postgres to start
-dc exec database_BAG update-atlas.sh
+dc exec -T database_BAG update-atlas.sh
 dc run --rm importer
 dc run --rm el-backup
 dc down 
