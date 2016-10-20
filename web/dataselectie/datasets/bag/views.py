8ae5410dd2ea5bac97afc432f4de4a88e37ca640
Kfir Breger <kfir@cubicalwave.com>
2016-10-20 16:29:04 +0200

    tg-146 doing some code cleanup

--- web/dataselectie/datasets/bag/views.py
+++ web/dataselectie/datasets/bag/views.py
@@ -99,6 +99,56 @@ class BagCSV(BagBase, CSVExportView):
     def elastic_query(self, query):
         return meta_q(query, add_aggs=False)
 
+
+    def create_geometry_dict(self, db_item):
+        """
+        Creates a geometry dict that can be used to add
+        geometry information to the result set
+
+        Returns a dict with geometry information if one
+        can be created. If not, an empty dict is returned
+        """
+        res = {}
+        try:
+            geom = db_item.adresseerbaar_object.geometrie.centroid
+        except AttributeError:
+            geom = None
+        if geom:
+            # Convert to wgs
+            geom_wgs = geom.transform('wgs84', clone=True).coords
+            geom = geom.coords
+            res = {
+                'geometrie_rd_x': int(geom[0]),
+                'geometrie_rd_y': int(geom[1]),
+                'geometrie_wgs_lat': ('{:.7f}'.format(geom_wgs[1])).replace('.', ','),
+                'geometrie_wgs_lon': ('{:.7f}'.format(geom_wgs[0])).replace('.', ',')
+            }
+        return res
+
+    def verblijfsobject_updates(self, item):
+        """
+        Creates a dict with verblijfsobject specific data
+        A dict is always returned, even if no data is found, in which case
+        an empty dict is returned
+        """
+        verblijfsobject = {}
+        verblijfsobject_data = ['gebruiksdoel_omschrijving', 'oppervlakte']
+        for meta in verblijfsobject_data:
+            verblijfsobject[meta] = getattr(item.verblijfsobject, meta, '')
+        try:
+            verblijfsobject['bouwblok'] = item.verblijfsobject.bouwblok.code
+        except AttributeError:
+            pass
+        try:
+            verblijfsobject['gebruik'] = item.verblijfsobject.gebruik.omschrijving
+        except AttributeError:
+            pass
+        try:
+            verblijfsobject['panden'] = '/'.join([i.landelijk_id for i in item.verblijfsobject.panden.all()])
+        except AttributeError:
+            pass
+        return verblijfsobject
+
     def _convert_to_dicts(self, qs):
         """
         Overwriting the default conversion so that location data
@@ -108,49 +158,11 @@ class BagCSV(BagBase, CSVExportView):
         data = []
         for item in qs:
             dict_item = self._model_to_dict(item)
-            geom_wgs = None
-            try:
-                geom = item.adresseerbaar_object.geometrie.centroid
-            except AttributeError:
-                geom = None
-            if geom:
-                # Convert to wgs
-                geom_wgs = geom.transform('wgs84', clone=True).coords
-                geom = geom.coords
-                dict_item.update({
-                    'geometrie_rd_x': int(geom[0]),
-                    'geometrie_rd_y': int(geom[1]),
-                    'geometrie_wgs_lat': ('{:.7f}'.format(geom_wgs[1])).replace('.', ','),
-                    'geometrie_wgs_lon': ('{:.7f}'.format(geom_wgs[0])).replace('.', ',')
-                })
-            # Changing true/flase to yes/no
-            dict_item['hoofdadres'] = 'Ja' if dict_item['hoofdadres'] else 'Nee'
-            # Adding Verblijfobject specifiek data
-            if item.verblijfsobject:
-                verblijfsobject_data = ['gebruiksdoel_omschrijving', 'oppervlakte']
-                for meta in verblijfsobject_data:
-                    dict_item[meta] = getattr(item.verblijfsobject, meta, '')
-                # Fetelijk gebruik
-                try:
-                    dict_item['bouwblok'] = item.verblijfsobject.bouwblok.code
-                except AttributeError:
-                    pass
-                try:
-                    dict_item['gebruik'] = item.verblijfsobject.gebruik.omschrijving
-                except AttributeError:
-                    pass
-                try:
-                    dict_item['panden'] = '/'.join([i.landelijk_id for i in item.verblijfsobject.panden.all()])
-                except AttributeError:
-                    pass
-                # Trying to update to landelijk id
-                landelijk_ids = ['openbare_ruimte', 'verblijfsobject', 'ligplaats', 'standplaats']
-                for sub_item_name in landelijk_ids:
-                    try:
-                        ref_item = getattr(item, sub_item_name, None)
-                        dict_item[sub_item_name] = ref_item.landelijk_id
-                    except:
-                        pass
+            # BAG Specific updates.
+            #------------------------
+            # Adding geometry
+            dict_item.update(self.create_geometry_dict(item))
+            # Updating status to description
             try:
                 dict_item['status'] = item.adresseerbaar_object.status.omschrijving
             except AttributeError:
@@ -158,6 +170,20 @@ class BagCSV(BagBase, CSVExportView):
             # Converting type
             type_code = int(dict_item['type']) - 1
             dict_item['type'] = self.model.OBJECT_TYPE_CHOICES[type_code][1]
+            # Changing true/flase to yes/no
+            dict_item['hoofdadres'] = 'Ja' if dict_item['hoofdadres'] else 'Nee'
+            # Updating to landelijk id
+            landelijk_ids = ['openbare_ruimte', 'adresseerbaar_object']
+            for sub_item_name in landelijk_ids:
+                try:
+                    ref_item = getattr(item, sub_item_name, None)
+                    dict_item[sub_item_name] = ref_item.landelijk_id
+                except:
+                    pass
+            # Adding Verblijfobject specifiek data
+            if item.verblijfsobject:
+                dict_item.update(self.verblijfsobject_updates(item))
+                    
             # Saving the dict
             data.append(dict_item)
         return data
