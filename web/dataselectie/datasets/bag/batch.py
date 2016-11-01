8a0172115cb396b3d009142f67bcbc49545c60f2
Kfir Breger <kfir@cubicalwave.com>
2016-11-01 08:51:17 +0100

    Removing unused if clause

--- web/dataselectie/datasets/bag/batch.py
+++ web/dataselectie/datasets/bag/batch.py
@@ -13,42 +13,35 @@ BAG_DOC_TYPES = (
 )
 
 
-class DeleteBagIndexTask(index.DeleteIndexTask):
+class DeleteDsBagIndexTask(index.DeleteIndexTask):
     index = settings.ELASTIC_INDICES['DS_BAG']
     doc_types = BAG_DOC_TYPES
 
 
-class IndexBagTask(index.ImportIndexTask):
+class IndexDsBagTask(index.ImportIndexTask):
     name = "index bag data"
-    if settings.IN_TEST_MODE:
-        queryset = models.Nummeraanduiding.objects. \
-            prefetch_related('verblijfsobject').\
-            prefetch_related('standplaats').\
-            prefetch_related('ligplaats').\
-            prefetch_related('openbare_ruimte')
-    else:
-        queryset = models.Nummeraanduiding.objects. \
-            prefetch_related('verblijfsobject').\
-            prefetch_related('standplaats').\
-            prefetch_related('ligplaats').\
-            prefetch_related('openbare_ruimte')
+    queryset = models.Nummeraanduiding.objects.\
+        prefetch_related('verblijfsobject').\
+        prefetch_related('standplaats').\
+        prefetch_related('ligplaats').\
+        prefetch_related('openbare_ruimte')
 
     def convert(self, obj):
         return documents.meta_from_nummeraanduiding(obj)
 
 
-class BuildIndexBagJob(object):
+class BuildIndexDsBagJob(object):
     name = "Create new search-index for all BAG data from database"
 
     @staticmethod
     def tasks():
         return [
-            DeleteBagIndexTask(),
-            IndexBagTask(),
+            DeleteDsBagIndexTask(),
+            IndexDsBagTask(),
         ]
 
 
-class DeleteIndexBagJob(object):
+class DeleteIndexDsBagJob(object):
     name = "Delete BAG related indexes"
 
     @staticmethod
