e204ac57429d6727e8ea592a0438a4fc8413369f
Stephan Zaat <s.zaat@amsterdam.nl>
2016-11-04 09:43:36 +0100

    Revert to download DB from yesterday

--- .jenkins/import.sh
+++ .jenkins/import.sh
@@ -14,9 +14,8 @@ mkdir -p ${DIR}/backups
 dc build --pull
 dc up -d database_BAG
 sleep 10 # waiting for postgres to start
-dc exec -T database_BAG dropdb --if-exists -U postgres atlas
-dc exec -T database_BAG createuser -U postgres atlas
-dc exec -T database_BAG pg_restore --if-exists -j 4 -c -C -d postgres -U postgres /mnt/dumps/bag.dump
+dc exec -T database_BAG update-db.sh atlas
+#dc exec -T database_BAG pg_restore -j 4 -c --if-exists -d postgres -U postgres /tmp/import/bag.dump
 
 dc run --rm importer
 dc run --rm el-backup
--- web/dataselectie/docker-wait.sh
+++ web/dataselectie/docker-wait.sh
@@ -7,19 +7,19 @@ set -e
 while ! nc -z ${ELASTICSEARCH_PORT_9200_TCP_ADDR} ${ELASTICSEARCH_PORT_9200_TCP_PORT}
 do
 	echo "Waiting for elastic..."
-	sleep 0.1
+	sleep 2
 done
 
 # wait for postgres BAG
 while ! nc -z ${DATABASE_BAG_PORT_5432_TCP_ADDR} ${DATABASE_BAG_PORT_5432_TCP_PORT}
 do
 	echo "Waiting for postgres..."
-	sleep 0.1
+	sleep 2
 done
 
 # wait for postgres dataselectie
 while ! nc -z ${DATABASE_DATASELECTIE_PORT_5432_TCP_ADDR} ${DATABASE_DATASELECTIE_PORT_5432_TCP_PORT}
 do
 	echo "Waiting for postgres..."
-	sleep 0.1
+	sleep 2
 done
