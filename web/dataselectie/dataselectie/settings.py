7eead283992c924e0cd31148ddd07fa0bcc628a2
Stephan Preeker <stephan@preeker.net>
2016-11-30 11:01:44 +0100

    debug acc

--- web/dataselectie/dataselectie/settings.py
+++ web/dataselectie/dataselectie/settings.py
@@ -35,6 +35,7 @@ insecure_key = 'insecure'
 SECRET_KEY = os.getenv('DATASELECTIE_SECRET_KEY', insecure_key)
 
 DEBUG = SECRET_KEY == insecure_key
+DEBUG = True
 
 ALLOWED_HOSTS = []  # type: List[str]
 SITE_ID = 1
