cae06c895a43d731bcf62631d29be548ba4bc1e3
Stephan Preeker <stephan@preeker.net>
2017-10-18 18:13:56 +0200

    tg-#1173 no more dry run

--- web/dataselectie/docker-index-bag.sh
+++ web/dataselectie/docker-index-bag.sh
@@ -4,9 +4,9 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 
 
-python manage.py elastic_indices bag --partial=1/30000 --build &
-python manage.py elastic_indices bag --partial=2/30000 --build &
-python manage.py elastic_indices bag --partial=3/30000 --build &
+python manage.py elastic_indices bag --partial=1/3 --build &
+python manage.py elastic_indices bag --partial=2/3 --build &
+python manage.py elastic_indices bag --partial=3/3 --build &
 
 FAIL=0
 
