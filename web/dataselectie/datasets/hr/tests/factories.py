c8465e887f9ca2ccdfbce2e1db789ca6f9d6dfe0
Marten Hoekstra <marten@dolhuis.nl>
2018-03-05 13:30:42 +0100

    Workaround ivm downgrade postgres-docker

--- web/dataselectie/datasets/hr/tests/factories.py
+++ web/dataselectie/datasets/hr/tests/factories.py
@@ -13,7 +13,7 @@ create_hr_table = """--
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
+-- SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
