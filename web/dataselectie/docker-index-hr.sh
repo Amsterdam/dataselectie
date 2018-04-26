72f0422aedcbbd85f9c497ba5fcd9ba4d3c1e3c7
Stephan Preeker <stephan@preeker.net>
2018-04-26 11:53:31 +0200

    -x in bash

--- web/dataselectie/docker-index-hr.sh
+++ web/dataselectie/docker-index-hr.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-set -u   # crash on missing env variables
-set -e   # stop on any error
+set -u   # crash on missing env variables.
+set -e   # stop on any error.
+set -x   # print what we are doing.
 
 
 python manage.py elastic_indices hr --partial=1/3 --build &
