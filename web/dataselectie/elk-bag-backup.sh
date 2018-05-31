42a0ea97ed2de731e2d96938456731d495bd917c
Bart Heupers <bart.heupers@gmail.com>
2018-05-31 10:25:39 +0200

    Add Content-Type: application/json foor ES6 PUT requests

--- web/dataselectie/elk-bag-backup.sh
+++ web/dataselectie/elk-bag-backup.sh
@@ -4,13 +4,13 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 
 
-curl -s -v -f -XPUT http://elasticsearch:9200/_snapshot/backup -d '
+curl -s -v -f -XPUT http://elasticsearch:9200/_snapshot/backup -H 'Content-Type: application/json' -d '
 {
   "type": "fs",
   "settings": {
       "location": "/tmp/backups" }
 }'
 
-curl -s -v -f -XPUT http://elasticsearch:9200/_snapshot/backup/ds_bag_index?wait_for_completion=true -d '
+curl -s -v -f -XPUT http://elasticsearch:9200/_snapshot/backup/ds_bag_index?wait_for_completion=true -H 'Content-Type: application/json' -d '
 { "indices": "ds_bag_index" }'
 
