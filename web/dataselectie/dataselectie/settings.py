c8d99915a54d7d629640fea07c2500a893c3fd04
Bart Heupers <bart.heupers@gmail.com>
2020-03-11 10:37:31 +0100

    Set sentry environment to dataselectie

--- web/dataselectie/dataselectie/settings.py
+++ web/dataselectie/dataselectie/settings.py
@@ -172,5 +172,6 @@ SENTRY_DSN = os.getenv('SENTRY_DSN')
 if SENTRY_DSN:
     sentry_sdk.init(
         dsn=SENTRY_DSN,
+        environment="dataselectie",
         integrations=[DjangoIntegration()]
     )
