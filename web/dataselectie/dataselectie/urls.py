e15b9ac9c9bae5e655a42e8bc10a558693c2ebec
jjmurre <jan.murre@gmail.com>
2023-12-12 08:08:08 +0100

    AB#100476  Disable HR and BRK endpoints
    To try this out for a short while.
    And detect is there are still users for these endpoints.

--- web/dataselectie/dataselectie/urls.py
+++ web/dataselectie/dataselectie/urls.py
@@ -25,8 +25,9 @@ def openapi(request):
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^dataselectie/bag/', include('datasets.bag.urls')),
-    url(r'^dataselectie/brk/', include('datasets.brk.urls')),
-    url(r'^dataselectie/hr/', include('datasets.hr.urls')),
+    # Issue AB 100476, disable HR and BRK for a while.
+    # url(r'^dataselectie/brk/', include('datasets.brk.urls')),
+    # url(r'^dataselectie/hr/', include('datasets.hr.urls')),
     url(r'^status/', include('health.urls')),
     url(r'^dataselectie/api-docs/openapi.yml', openapi)
 ]
